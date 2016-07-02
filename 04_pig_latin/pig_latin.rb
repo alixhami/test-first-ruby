b1e1dc20046f92f8f29b37d488c9115866985f4c
Alix Hamilton <alix.hamilton@gmail.com>
2016-07-01 22:22:30 -0700

    solved 04 pig latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,14 @@
 #write your code here
+def translate(phrase)
+	words = phrase.split
+	words.each do |word|
+		vowels = ['a','e','i','o','u']
+		if vowels.include?(word[0])
+			word.replace word + "ay"
+		else
+	    first_vowel = word.index(/[aeio]/)
+			word.replace word[first_vowel..-1] + word[0..first_vowel-1] + "ay"
+		end
+	end
+	words.join(" ")
+end
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,4 @@
 class Book
 # write your code here
+  def title
 end
