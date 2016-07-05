919565e28c79df43e6702597ed84041099d23229
Alix Hamilton <alix.hamilton@gmail.com>
2016-07-04 22:48:32 -0700

    solved 04_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,4 +1,22 @@
 class Book
 # write your code here
+  attr_reader :title
+
+  def title=(name)
+  	words = name.split
+  	little_words = ['the','a','an','and','in','of','to']
+    words[0].capitalize!
+  	words[1..-1].map do |word|
+  		if little_words.include? word
+  			word
+      else
+        word.capitalize!
+      end
+  	end
+  	@title = words.join(' ')
+  end
+
   def title
+    @title
+  end
 end
